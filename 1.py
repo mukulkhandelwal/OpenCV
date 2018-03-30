2b05dad841f8d6215b8f614754240c687cb2350e
Mukul <mukulkhandelwal1997@gmail.com>
2018-03-30 15:27:19 +0530

    some changes

--- 1.py
+++ 1.py
@@ -7,7 +7,16 @@ img = cv2.imread('a2.jpeg', cv2.IMREAD_GRAYSCALE)
 #IMREAD_COLOR = 1
 #IMREAD_UNCHANGED  = -1 
 
-cv2.imshow('image',img)
+# cv2.imshow('image',img)
 
-cv2.waitKey(0)
-cv2.destroyALlWindows();
+# cv2.waitKey(0)
+# cv2.destroyALlWindows();
+
+plt.imshow(img, cmap='gray', interpolation =
+	'bicubic')
+
+plt.plot([50,100],[80,100],'c',linewidth=5)
+plt.show()
+
+
+#cv2.imwrite('xyz.png',img)
